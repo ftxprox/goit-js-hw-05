f1bc4f74b3d56f6d9cc88dbebcf1ad744ed379d7
ftxprox <168733166+ftxprox@users.noreply.github.com>
2024-08-13 19:45:21 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername(newName) {
+    profile.username = newName;
+  },
+  updatePlayTime(hours) {
+    profile.playTime += hours;
+  },
+  getInfo() {
+    return `${profile.username} has ${profile.playTime} active hours!`;
+  },
+};
+
+// function changeUsername(newName) {
+//   profile.username = newName;
+// }
+
+// function updatePlayTime(hours) {
+//   profile.playTime += hours;
+// }
+
+// function getInfo() {
+//   return `${profile.username} has ${profile.playTime} active hours!`;
+// }
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
